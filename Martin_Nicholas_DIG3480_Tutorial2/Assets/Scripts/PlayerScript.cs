6b19a476e6f4832194c786f479ef1b75cde04058
weirdoofcool <59780020+weirdoofcool@users.noreply.github.com>
2020-03-08 23:50:35 -0400

    My version of DIG3480's Challenge2
    DIG3480 Challenge 2

--- Martin_Nicholas_DIG3480_Tutorial2/Assets/Scripts/PlayerScript.cs
+++ Martin_Nicholas_DIG3480_Tutorial2/Assets/Scripts/PlayerScript.cs
@@ -11,13 +11,44 @@ public class PlayerScript : MonoBehaviour
 
     public Text score;
 
+    public Text WinText;
+
+    public Text LiveText;
+
     private int scoreValue = 0;
+
+    private int LiveTextValue = 3;
+
+    public Text LoseText;
+
+    public AudioSource musicSource;
+
+    public AudioClip musicClipOne;
+
+    public AudioClip musicClipTwo;
+
+    Animator anim;
+
+    private bool facingRight = true;
+
+    private bool isOnGround;
+
+    public Transform groundcheck;
+
+    public float checkRadius;
+
+    public LayerMask allGround;
 
     // Start is called before the first frame update
     void Start()
     {
         rd2d = GetComponent<Rigidbody2D>();
         score.text = scoreValue.ToString();
+        WinText.text = "";
+        LiveText.text = LiveTextValue.ToString();
+        LoseText.text = "";
+        anim = GetComponent<Animator>();
+        isOnGround = Physics2D.OverlapCircle(groundcheck.position, checkRadius, allGround);
     }
 
     void Update()
@@ -27,15 +58,38 @@ public class PlayerScript : MonoBehaviour
             Application.Quit();
         }
 
-    }
+        if(Input.GetKeyDown(KeyCode.A))
+        { anim.SetInteger("State", 1); }
 
+        if(Input.GetKeyUp(KeyCode.A))
+        { anim.SetInteger("State", 0); }
+
+        if(Input.GetKeyDown(KeyCode.D))
+        { anim.SetInteger("State", 1); }
+
+        if(Input.GetKeyUp(KeyCode.A))
+        { anim.SetInteger("State", 0); }
+
+
+    }
     // Update is called once per frame
     void FixedUpdate()
     {
         float hozMovement = Input.GetAxis("Horizontal");
         float vertMovement = Input.GetAxis("Vertical");
         rd2d.AddForce(new Vector2(hozMovement * speed, vertMovement * speed));
+
+        if (facingRight == false && hozMovement > 0)
+        {
+            Flip();
+        }
+        else if (facingRight == true && hozMovement < 0)
+        {
+            Flip();
+        }
     }
+
+   
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
@@ -44,6 +98,34 @@ public class PlayerScript : MonoBehaviour
             scoreValue += 1;
             score.text = scoreValue.ToString();
             Destroy(collision.collider.gameObject);
+            if (scoreValue >= 8)
+            {
+                WinText.text = "You win! Game created by Nicholas Martin";
+                musicSource.Stop();
+                musicSource.clip = musicClipTwo;
+                musicSource.loop = false;
+                musicSource.Play();
+                    }
+            
+            if (scoreValue == 4)
+            {
+                transform.position = new Vector2(31.18f, -2.39f);
+                LiveTextValue = 3;
+            }
+        }
+
+
+
+        if (collision.collider.tag == "Enemy")
+        {
+            LiveTextValue -= 1;
+            LiveText.text = LiveTextValue.ToString();
+            Destroy(collision.collider.gameObject);
+            if (LiveTextValue <= 0)
+            {
+                LoseText.text = "You Lose";
+                Destroy(this);
+            }
         }
 
     }
@@ -54,8 +136,27 @@ public class PlayerScript : MonoBehaviour
         {
             if (Input.GetKey(KeyCode.W))
             {
-                rd2d.AddForce(new Vector2(0, 3), ForceMode2D.Impulse);
+                rd2d.AddForce(new Vector2(0, 1), ForceMode2D.Impulse);
+            }
+
+        }
+        {
+            if (collision.collider.tag == "Ground" && isOnGround)
+            {
+                if (Input.GetKey(KeyCode.W))
+                {
+                    rd2d.AddForce(new Vector2(0, 3), ForceMode2D.Impulse);
+                }
             }
         }
     }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector2 Scaler = transform.localScale;
+        Scaler.x = Scaler.x * -1;
+        transform.localScale = Scaler;
+    }
+
 }
